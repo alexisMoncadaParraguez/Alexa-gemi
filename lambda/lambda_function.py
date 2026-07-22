3a6d394735e37b94b541fa5bb0815af780b3c138
rashiraku <alexismp18@gmail.com>
2026-07-22 01:19:18 -0400

    cambio a es_ESP

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -34,7 +34,7 @@ class LaunchRequestHandler(AbstractRequestHandler):
         return ask_utils.is_request_type("LaunchRequest")(handler_input)
 
     def handle(self, handler_input):
-        speak_output = "Yemi activado. Cual es tu pregunta?"
+        speak_output = "Dime"
         return (
             handler_input.response_builder.speak(speak_output)
             .ask(speak_output)
